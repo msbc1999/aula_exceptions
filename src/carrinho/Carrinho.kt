46487abd552eebdc7380b90c7682aeee5265b286
Mateus <mateustrindade8@gmail.com>
2020-09-21 20:11:49 -0300

    Classe carrinho

---
+++ src/carrinho/Carrinho.kt
@@ -0,0 +1,10 @@
+class Carrinho(var produtos: MutableList<Produto>) {
+
+    fun valorTotal(){
+        var somaTotal = 0.0
+        produtos.forEach(){
+            somaTotal += it.getValor()
+        }
+        println("O valor total do carrinho é R$$somaTotal")
+    }
+}
